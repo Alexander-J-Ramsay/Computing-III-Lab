0d1feb333989ed7ab71b6bb9d6c3b13bbfc02fca
Alexander-J-Ramsay <Alexander_Ramsay@student.uml.edu>
2026-09-08 16:17:21 -0400

    Enhance loan calculation with user inputs
    Added loan amount and period inputs, and calculated total amount and monthly payments.

--- Lab-1.cpp
+++ Lab-1.cpp
@@ -5,12 +5,30 @@
 using namespace std;
 
 double AIR;
+int loan_amount;
+int loan_period;
+
 
 int main()
 {
     cout << "Please enter the annual interest rate as a percentage (e.g., 15 for 15%) (enter 0 to quit): " << endl;
     cin >> AIR;
-    AIR = AIR / 100;
-    cout << AIR;
+    AIR /= 100;
+
+    cout << "Please enter the amount you would like to receive: " << endl;
+    cin >> loan_amount;
+
+    cout << "Please enter loan period in months:" << endl;
+    cin >> loan_period;
+
+
+    double total_interest = loan_amount * AIR * (loan_period / 12);
+    double total_amount = loan_amount + total_interest;
+    double monthly_amount = total_amount / loan_period;
+
+    cout << "the total amount of your loan (including interest at 15% per annum) is $" << total_amount << endl;
+    cout << "Your monthly payment for 12 months will be $" << monthly_amount << endl;
+
+  
 }
 
