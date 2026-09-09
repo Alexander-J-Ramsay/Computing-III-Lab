282e0afff824185e8d114d7aa23af41fe0bb2919
masondefran-hub <masondefran@gmail.com>
2026-09-08 23:19:14 -0400

    Update Lab-1.cpp
    added in the loop, fixed formatting and variable names, added in rounding to the nearest cent for both monthly due and total, added comments, added a return 0 at the end and some other stuff

--- Lab-1.cpp
+++ Lab-1.cpp
@@ -1,31 +1,42 @@
-// Lab-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cmath> //cmath for the rounding.
+
 using namespace std;
 
-int main()
+int main(void)
 {
-    double AIR;
+    double intrest;
     double loan_amount;
     int loan_period;
+    double years;
+    double loan_total;
+    double monthly_amount;
 
-    cout << "Please enter the annual interest rate as a percentage (e.g., 15 for 15%) (enter 0 to quit): " ;
-    cin >> AIR;
-    AIR /= 100;
+    //First check from the user for if they want to run the program and enter the loop.
+    cout << "Please enter the annual interest rate as a percentage (e.g., 15 for 15%) (enter 0 to quit): ";
+    cin >> intrest;
 
-    cout << "Please enter the amount you would like to receive: ";
-    cin >> loan_amount;
+    while (intrest != 0) //Loop continues until the user stops by entering intrest as 0.
+    {
+        intrest /= 100; //Convert the intrest to a decimal value.
 
-    cout << "Please enter loan period in months:";
-    cin >> loan_period;
+        cout << "Please enter the amount you would like to receive: ";
+        cin >> loan_amount;
 
-    double years = loan_period / 12;
+        cout << "Please enter loan period in months: ";
+        cin >> loan_period;
 
-    double FVI = loan_amount / (1 - AIR * years);
-    double monthly_amount = FVI / loan_period;
+        years = loan_period / 12; //Convert months into years as a decimal value.
 
-    cout << "the total amount of your loan (including interest at 15% per annum) is $" << FVI << endl;
-    cout << "Your monthly payment for 12 months will be $" << monthly_amount << endl;
+        //Calculate the overall loan total and monthly amount after intrest rounding to the nearest cent.
+        loan_total = round((loan_amount / (1 - intrest * years)) * 100) / 100; 
+        monthly_amount = round((loan_total / loan_period) * 100) / 100; 
 
+        cout << "the total amount of your loan (including interest at 15% per annum) is $" << loan_total << "." << endl;
+        cout << "Your monthly payment for 12 months will be $" << monthly_amount << "." << "\n" << endl;
+
+        cout << "Please enter the annual interest rate as a percentage (e.g., 15 for 15%) (enter 0 to quit): ";
+        cin >> intrest;
+    }
+    return 0; //Terminate program once user inputs 0 for intrest.
 }
