01bb673bf52937450a2f4039ac299f5299c25ec0
sashimi-z <aidenzlatkov1@gmail.com>
2026-09-09 18:53:23 -0400

    fixed "interest" typos, "intrest" -> "interest"

--- Lab-1.cpp
+++ Lab-1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(void)
 {
-    double intrest;
+    double interest;
     double loan_amount;
     int loan_period;
     double years;
@@ -14,11 +14,11 @@ int main(void)
 
     //First check from the user for if they want to run the program and enter the loop.
     cout << "Please enter the annual interest rate as a percentage (e.g., 15 for 15%) (enter 0 to quit): ";
-    cin >> intrest;
+    cin >> interest;
 
-    while (intrest != 0) //Loop continues until the user stops by entering intrest as 0.
+    while (interest != 0) //Loop continues until the user stops by entering interest as 0.
     {
-        intrest /= 100; //Convert the intrest to a decimal value.
+        interest /= 100; //Convert the interest to a decimal value.
 
         cout << "Please enter the amount you would like to receive: ";
         cin >> loan_amount;
@@ -28,15 +28,15 @@ int main(void)
 
         years = loan_period / 12; //Convert months into years as a decimal value.
 
-        //Calculate the overall loan total and monthly amount after intrest rounding to the nearest cent.
-        loan_total = round((loan_amount / (1 - intrest * years)) * 100) / 100; 
+        //Calculate the overall loan total and monthly amount after interest rounding to the nearest cent.
+        loan_total = round((loan_amount / (1 - interest * years)) * 100) / 100; 
         monthly_amount = round((loan_total / loan_period) * 100) / 100; 
 
         cout << "the total amount of your loan (including interest at 15% per annum) is $" << loan_total << "." << endl;
         cout << "Your monthly payment for 12 months will be $" << monthly_amount << "." << "\n" << endl;
 
         cout << "Please enter the annual interest rate as a percentage (e.g., 15 for 15%) (enter 0 to quit): ";
-        cin >> intrest;
+        cin >> interest;
     }
-    return 0; //Terminate program once user inputs 0 for intrest.
+    return 0; //Terminate program once user inputs 0 for interest.
 }
