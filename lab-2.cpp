d73a30b4f0da244e1795ddd7071e7290a3da4412
sashimi-z <aidenzlatkov1@gmail.com>
2026-09-15 15:41:53 -0400

    Fix formatting issues in lab-2.cpp

--- lab-2.cpp
+++ lab-2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+
 void getInput(int &pounds, double &ounces); // Get user input for pounds and ounces
 
 int main()
@@ -16,8 +17,5 @@ void getInput(int &pounds, double &ounces)
     cin >> pounds;
     cout << "Please enter the number of ounces: ";
     cin >> ounces;
-    
-    //cout << pounds << endl;
-    //cout << ounces << endl;
 }
 
