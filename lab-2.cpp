35b78a8ce21b47c4e45a0ae3590c92022393364d
Alexander-J-Ramsay <Alexander_Ramsay@student.uml.edu>
2026-09-15 16:08:50 -0400

    Implement conversion from pounds and ounces to metric
    Added calculation function to convert pounds and ounces to kilograms and grams.

--- lab-2.cpp
+++ lab-2.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 
 void getInput(int &pounds, double &ounces); // Get user input for pounds and ounces
+void calculation(double ounces, double pounds, double& kilograms, double& grams);
 
 int main()
 {
@@ -17,5 +19,15 @@ void getInput(int &pounds, double &ounces)
     cin >> pounds;
     cout << "Please enter the number of ounces: ";
     cin >> ounces;
+}
+
+void calculation(double ounces, double pounds, double& kilograms, double& grams)
+{
+    double pounds_Combined = pounds + (ounces / 16);
+
+    double total_kilograms = pounds_Combined * 0.45359237;
+    kilograms = static_cast<int>(floor(total_kilograms));
+
+    grams = (total_kilograms - kilograms) * 1000;
 }
 
