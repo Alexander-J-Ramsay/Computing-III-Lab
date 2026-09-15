c4b571a81525f525f8dd6d9909f1d4e5fc15e682
sashimi-z <aidenzlatkov1@gmail.com>
2026-09-15 15:40:20 -0400

    Implement user input function for pounds and ounces
    Added a function to get user input for pounds and ounces.

--- lab-2.cpp
+++ lab-2.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <cmath>
+
+void getInput(int &pounds, double &ounces); // Get user input for pounds and ounces
 
 int main()
 {
+    int pounds;
+    double ounces;
+    getInput(pounds, ounces);
     
+}
+void getInput(int &pounds, double &ounces)
+{
+    cout << "Please enter the number of pounds: ";
+    cin >> pounds;
+    cout << "Please enter the number of ounces: ";
+    cin >> ounces;
+    
+    //cout << pounds << endl;
+    //cout << ounces << endl;
 }
 
