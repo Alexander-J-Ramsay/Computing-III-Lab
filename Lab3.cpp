fa63df9c41b34819f39d5708a2714ee8de712cd8
Alexander-J-Ramsay <Alexander_Ramsay@student.uml.edu>
2026-09-22 16:30:06 -0400

    branch testing

--- Lab3.cpp
+++ Lab3.cpp
@@ -1,51 +1,96 @@
+// lab_3
+
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-void runConversions(double drams);
+class Mass {
+public:
+	void setMassAvoirdupoisPounds(double avoirdupois_Pounds);
+	void setMassTroyPounds(double troy_Pounds);
+	void setMassMetricGrams(double metric_Grams);
+	double get_avoirdupoisPounds(void);
+	double get_troyPounds(void);
+	double get_metricGrams(void);
 
-int main(void)
-{
-
-	int inputSelection = -1;
-	double inputMass;
+private:
 	double drams;
 
-	while (inputSelection != 0)
-	{
-		cout << "Please enter 1 to use Avoirdupois pounds, 2 to use Troy pounds, 3 to use grams, or 0 to exit: ";
-		cin >> inputSelection;
+};
 
-		switch (inputSelection)
-		{
-		case 1:
-			cout << "Please enter a mass in Avoirdupois pounds: ";
-			cin >> inputMass;
-			drams = inputMass * 256.0;
-			break;
-		case 2:
-			cout << "Please enter a mass in Troy pounds: ";
-			cin >> inputMass;
-			drams = inputMass * 96.0;
-			break;
-		case 3:
-			cout << "Please enter a mass in grams: ";
-			cin >> inputMass;
-			drams = inputMass / 1.7718451953125;
-			break;
-		default:
-			cout << "\nThanks for using the mass conversion program!" << endl;
-			exit(0);
-			break;
-		}
-		runConversions(drams);
-	}
-	return 0;
+void Mass::setMassAvoirdupoisPounds(double avordupois_Pounds) 
+{
+	drams = avordupois_Pounds * 256;
 }
 
-void runConversions(double drams)
+void Mass::setMassTroyPounds(double troy_Pounds)
 {
-	cout << "Mass in Avoirdupois pounds is: " << drams / 256 << endl;
-	cout << "Mass in Troy pounds is: " << drams / 96 << endl;
-	cout << "Mass in grams is: " << drams * 1.7718451953125 << "\n" << endl;
+	drams = troy_Pounds * 96;
+}
+
+void Mass::setMassMetricGrams(double metric_Grams)
+{
+	drams = metric_Grams / 1.7718451953125;
+}
+
+double Mass::get_avoirdupoisPounds(void)
+{
+	return drams / 256;
+}
+
+double Mass::get_troyPounds(void)
+{
+	return drams / 96;
+}
+
+double Mass::get_metricGrams(void)
+{
+	return drams * 1.7718451953125;
+}
+
+
+
+
+
+int main()
+{
+	int choice;
+	int choice2;
+	Mass weight;
+
+	do
+	{
+		cout << "Please enter 1 to use Avoirdupois pounds, 2 to use Troy pounds, 3 to use grams, or 0 to exit : ";
+		cin >> choice;
+
+		if (choice == 1)
+		{
+			cout << "Please enter a mass in Avoirdupois pounds: ";
+			cin >> choice2;
+
+			weight.setMassAvoirdupoisPounds(choice2);
+			cout << "troy pounds --> " << weight.get_troyPounds() << "    metric grams --> " << weight.get_metricGrams() << endl;
+		}
+		else if (choice == 2)
+		{
+			cout << "Please enter a mass in troy pounds: ";
+			cin >> choice2;
+
+			weight.setMassTroyPounds(choice2);
+			cout << "avoirdupois pounds --> " << weight.get_avoirdupoisPounds() << "   metric grams --> " << weight.get_metricGrams() << endl;
+		}
+		else if (choice == 3)
+		{
+			cout << "Please enter a mass in metric grams: ";
+			cin >> choice2;
+
+			weight.setMassMetricGrams(choice2);
+			cout << "troy pounds --> " << weight.get_troyPounds() << "   avoirdupois pounds --> " << weight.get_avoirdupoisPounds() << endl;
+		}
+		else
+		{
+			break;
+		}
+	} while (choice != 0);
 }
